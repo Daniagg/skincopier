d8dc36a37909806af63657bb90d18b78713ecba7
Daniagg <daniagg1506@gmail.com>
2025-04-02 06:42:32 +0700

    Create 123.lua

---
+++ 123.lua
@@ -0,0 +1,10 @@
+local player = game.Players.LocalPlayer
+
+local value = Instance.new("NumberValue")
+value.Name = "gfe3"
+value.Value = 0
+value.Parent = player
+
+--task.wait(1)
+
+--value.Value = 1
